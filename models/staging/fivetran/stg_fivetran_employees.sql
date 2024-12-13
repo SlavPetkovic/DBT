5f2eda560c6db07bbce001f26d0eb2ceb530b354
Slavoljub Petkovic <dev.petkovic@outlook.com>
2024-12-13 08:38:59 +0000

    create employee stg model

---
+++ models/staging/fivetran/stg_fivetran_employees.sql
@@ -0,0 +1,25 @@
+with source as (
+
+    select * from {{ source('fivetran', 'EMPLOYEES') }}
+
+),
+
+renamed as (
+
+    select
+
+        _LINE as line_number,
+        _FIVETRAN_SYNCED as fivetran_synced,
+        EMP_NO as emp_no,
+        EMP_TITLE_ID as emp_title_id,
+        BIRTH_DATE as birth_date,
+        FIRST_NAME as first_name,
+        LAST_NAME as last_name,
+        SEX as sex,
+        HIRE_DATE as hire_date
+
+    from source
+
+)
+
+select * from renamed
